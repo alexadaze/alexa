3b9af03bd5f4ba0700f0340980cc322c9a1ddeb8
Bryan Weber <bryan@cobenian.com>
2017-02-18 22:22:47 -0500

    removing static manifest config

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :alexa, Alexa.Endpoint,
   # url: [host: "example.com", port: 80],
   url: [scheme: "https", host: "mysterious-island-82168.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  # cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # # Configure your database
