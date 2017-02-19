99ccc01c516e3e94099338c712e89e9bb07ddc2f
Bryan Weber <bryan@cobenian.com>
2017-02-18 22:25:51 -0500

    returning json

--- web/controllers/alexa_controller.ex
+++ web/controllers/alexa_controller.ex
@@ -2,15 +2,16 @@ defmodule Alexa.AlexaController do
   use Alexa.Web, :controller
 
   def teamsnap(conn, _params) do
-    %{
-          "version" => "1.0",
-          "response" => %{
-            "outputSpeech" => %{
-              "type" => "PlainText",
-              "text" => "Hello World!",
-            },
-            "shouldEndSession" => true
-        }
-    }
+    json conn,
+      %{
+            "version" => "1.0",
+            "response" => %{
+              "outputSpeech" => %{
+                "type" => "PlainText",
+                "text" => "Hello World!",
+              },
+              "shouldEndSession" => true
+          }
+      }
   end
 end
